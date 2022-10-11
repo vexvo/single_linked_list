f43252f04ddee3253c90daa32409a6d54ed837d3
vexvo <santiago.uribeg@autonoma.edu.co>
2022-10-11 16:55:39 -0400

    insert

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from sll import SingleLinkedList
 
 linked_list = SingleLinkedList()
 
-linked_list.push_node('A')
+linked_list.append('A')
 linked_list.print_values()
-linked_list.insert('B')
+linked_list.appbegin('B')
 linked_list.print_values()
-linked_list.pop_node()
+linked_list.insert(1, 'C')
 linked_list.print_values()
